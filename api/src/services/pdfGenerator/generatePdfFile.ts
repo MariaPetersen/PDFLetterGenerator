17adab0891cc030b7badf444696d6258302db586
Maria Petersen <maria@strime.fr>
2024-04-06 15:38:44 +0200

    add teplates to build folder

--- api/src/services/pdfGenerator/generatePdfFile.ts
+++ api/src/services/pdfGenerator/generatePdfFile.ts
@@ -4,7 +4,8 @@ const puppeteer = require("puppeteer");
 const path = require("path");
 
 const generatePdf = async (templateFileName: string, data: ILetterData) => {
-  const templateFile = path.resolve(__dirname, "templates", templateFileName);
+  const templateFile = path.resolve(__dirname, "../../templates", templateFileName);
+  console.log(templateFile);
   return new Promise((resolve, reject) => {
     try {
       ejs.renderFile(
@@ -15,19 +16,14 @@ const generatePdf = async (templateFileName: string, data: ILetterData) => {
           if (err) {
             throw new Error("Something went wrong when generating PDF");
           }
-          console.log("here");
           const browser = await puppeteer.launch({
             headless: "new",
           });
-          console.log("here");
           const page = await browser.newPage();
-          console.log("here");
           await page.setContent(html);
-          console.log("here");
           const pdf = await page.pdf({
             format: "A4",
           });
-          console.log("here");
           await browser.close();
           resolve(pdf);
         }
