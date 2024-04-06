69b471b39891389f2446ca8c1c39fd272c71a818
Maria Petersen <maria@strime.fr>
2024-04-06 15:17:04 +0200

    fix env typo

--- api/src/services/database/database.ts
+++ api/src/services/database/database.ts
@@ -1,8 +1,8 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.USER,
-  password: process.env.PASSWORD,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: 5432,
   database: process.env.DATABASE,
